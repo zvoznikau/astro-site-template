4ce57ce2441ade29d395d448961702b774187b9a
Zvoznikau Vadzim <zvoznikoff@gmail.com>
2026-01-07 13:44:15 +0100

    feat: add tailwindcss and daisyui to astro app

---
+++ apps/web/astro/tailwind.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ['light', 'dark'],
+  },
+};
